d3f9dc355d7f5e57f8e20bb7d0fe644e1b5a60ab
Abdullah <abdullahmhm@aucegypt.edu>
2024-03-25 20:16:01 +0200

    jojo

--- main.cpp
+++ main.cpp
@@ -1,29 +1,42 @@
-#include <QGraphicsView>
-#include<QGraphicsScene>
+
 #include <QApplication>
-#include <QScrollBar>
-#include <QGraphicsRectItem>>
-using namespace Qt;
+#include <QGraphicsRectItem>
+#include <QGraphicsScene>
+#include<QGraphicsView>
+#include "player.h"
+#include <QTimer>
+#include <QDebug>
+#include <QBrush>
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    QGraphicsView View;
-    View.setFixedSize(800,600);
-    QGraphicsScene Scene;
-    Scene.setSceneRect(0,0,800,600);
-    View.setScene(&Scene);
-    View.show();
-    //Disabling Scroll Bars
-    View.setVerticalScrollBarPolicy(ScrollBarAlwaysOff);
-    View.setHorizontalScrollBarPolicy(ScrollBarAlwaysOff);
 
-    //Creating Item
-    QGraphicsRectItem *rect = new QGraphicsRectItem;
-    rect->setRect(100,300,200,200);
-    Scene.addItem(rect);
-    View.setScene(&Scene);
-    View.show();
+    // *******  Create the View ********
+    QGraphicsView view;
 
+    // ******* Create the Scene ********
+    QGraphicsScene scene;
+    view.setScene(&scene);
+
+    // *******  Create the Player ********
+    Player *player = new Player();
+    scene.addItem(player);
+
+    // *******  Setting the foucs to the Player ********
+    player->setFocus();
+
+    // *******  Adjust the location of the Player (middle of the screen) ********
+    player->setPos(view.width()/2 - player->boundingRect().width()/2,view.height()/2 - player->boundingRect().height()/2);
+
+
+   // *******   Assign scene to the view   ***************
+    view.setScene(&scene);
+
+    // *******  Create the Enemies automatically ********
+    QTimer * time = new QTimer();
+    QObject::connect(time, SIGNAL(timeout()),player,SLOT(createEnemy()));
+    time->start(2000);
 
 
     return a.exec();
